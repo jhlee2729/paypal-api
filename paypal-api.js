439b7eb528644e2c5885b7dafee057741c49a25f
jhlee2729 <wkdgml7644@gmail.com>
2023-06-23 20:50:58 +0900

    start_date, end_date 체크 후 DB 저장

--- paypal-api.js
+++ paypal-api.js
@@ -17,6 +17,10 @@ const contents = {
     end_date: ''
 }
 
+const insertData = {
+    transaction_info:[],
+}
+
 const execute = (sql,callback,data = {} )=>{
 
     pool.getConnection((err,connection) => {
@@ -41,7 +45,6 @@ const lastApiHistory = () => {
 
     return new Promise((resolve,reject) => {
 
-        //2014-07-01T00:00:00-0700
         execute(`SELECT * FROM app_paypal_api_history\
             WHERE paypal_id = ${syncData.paypal_id}
             ORDER BY api_history_id DESC LIMIT 1`, (err,rows) => {
@@ -49,10 +52,11 @@ const lastApiHistory = () => {
             if (err) throw err;
 
             if( rows.length >= 1) {
-                contents.start_date = rows[0].start_date;
+                contents.start_date = new Date(rows[0].end_date).setHours(new Date(rows[0].end_date).getHours()-9);
                 resolve();
             } else {
-                contents.start_date = settingDate.start_date;
+                //시간 맞춰주기 : 설정시간 - 9시간
+                contents.start_date = new Date(settingDate.start_date).setHours(new Date(settingDate.start_date).getHours()-9);
                 resolve();
 
             }
@@ -67,15 +71,20 @@ const dateCheck = () => {
 
         // 기본 end_date = start_date + 2주
         contents.end_date = new Date(contents.start_date).setDate(new Date(contents.start_date).getDate()+14);
+        // 현재시간에서 4시간 전까지 조회 : UTC - 9시간 반영 : -13 -> end_date : 현재에서 -4시간 전까지 부르겠다는 의미임
+        contents.now = new Date().setHours(new Date().getHours()-13);
 
-        contents.start_date = dateformat(contents.start_date, `yyyy-mm-dd'T'HH:MM:ss+0000`);
-        contents.end_date = dateformat(contents.end_date, `yyyy-mm-dd'T'HH:MM:ss+0000`);
-        contents.now = dateformat(new Date(), `yyyy-mm-dd'T'HH:MM:ss+0000`);
-        console.log(`now:${contents.now}, start_date:${contents.start_date}, end:${contents.end_date}`, contents.now < contents.end_date);
+        contents.start_date = dateformat(contents.start_date, `yyyy-mm-dd'T'HH:MM:ss-0000`);
+        contents.end_date = dateformat(contents.end_date, `yyyy-mm-dd'T'HH:MM:ss-0000`);
+        contents.now = dateformat(contents.now, `yyyy-mm-dd'T'HH:MM:ss-0000`);
 
-        if (contents.now < contents.end_date) {
+        console.log(`now:${contents.now}`)
+        console.log(`start_date:${contents.start_date}, end:${contents.end_date}`, contents.now < contents.end_date);
+
+        if (contents.now < contents.end_date) { //설정한 end 시간이 현재시간보다 더클경우
             contents.end_date = contents.now
         }
+
         resolve();
     })
 }
@@ -84,13 +93,11 @@ const getTransaction = () => {
 
     return new Promise((resolve,reject) => {
 
-        let start_date = contents.start_date;
-        let end_date = contents.end_date;
-
         let page_size = 500; // limit
         let page = 1;
 
         const callAPI = () => {
+
             axios({
                 method: 'GET',
                 url: 'https://api-m.paypal.com/v1/reporting/transactions',
@@ -99,8 +106,8 @@ const getTransaction = () => {
                     "Authorization": `Bearer ${syncData.access_token}`,
                 },
                 params: {
-                    start_date:start_date, 
-                    end_date:end_date,
+                    start_date:contents.start_date, 
+                    end_date:contents.end_date,
                     fields:'all',
                     page_size:page_size,
                     page:page
@@ -108,40 +115,23 @@ const getTransaction = () => {
           
             }).then((response) => {
 
-                response.data.transaction_details.map(i => { 
-                    console.log(i.transaction_info.transaction_id, i.transaction_info.transaction_updated_date )
-                })
+                // response.data.transaction_details.map(i => { 
+                //     console.log(i.transaction_info.transaction_id, i.transaction_info.transaction_updated_date )
+                // })
 
-                console.log("response", response.data.start_date)
-                console.log("response", response.data.end_date)
-
-                // console.log("response.start_date", response.data.start_date);
-                // console.log("response.end_date", response.data.end_date);
-                // console.log("첫번쨰 inovice",  response.data.transaction_details[0])
-                // console.log("2번째 데이터", insertData.transaction_info[page].transaction_info.transaction_id)
-
+                let res_start_date = response.data.start_date;
+                let res_end_date = response.data.end_date;
                 let total_items = response.data.total_items;
                 let total_pages = response.data.total_pages;
-
+                
+                console.log(`호출 : ${contents.start_date}, ${contents.end_date}`)
+                console.log(`응답: ${res_start_date}, ${res_end_date}`);
                 console.log(`total_items: ${total_items},total_pages :${total_pages}, page:${page}, ${total_pages!==page}`);
                 
-                // response.data.transaction_details.map(i => {
-                //     insertData.transaction_info = insertData.transaction_info.concat(i);
+                response.data.transaction_details.map(i => {
+                    insertData.transaction_info = insertData.transaction_info.concat(i);
+                })
 
-                //     // if(i.transaction_info.transaction_id == '7SX72684TW306463B') {
-                //     //     console.log(i.transaction_info)
-                //     // }
-
-                //     // console.log(i.transaction_info.transaction_id);
-                //     // console.log(i.transaction_info.invoice_id);
-                //     // console.log(i.transaction_info.transaction_initiation_date);
-                //     // console.log(i.transaction_info.transaction_updated_date);
-                //     // console.log(i.transaction_info.transaction_amount.value);
-                //     // console.log((i.transaction_info.hasOwnProperty('fee_amount') && i.transaction_info.fee_amount.value) || 0);
-                //     // console.log(i.transaction_info.transaction_status);
-                //     // console.log("======================")
-                // })
-                
                 if ( (total_pages !== page) && total_items !==0 ) {
                     ++page;
                     callAPI();
@@ -152,7 +142,8 @@ const getTransaction = () => {
                 }
                
             }).catch((err)=>{
-                console.log("err", err);
+                console.log("err", err.response.data);
+                resolve(false);
             });
         }
 
@@ -161,21 +152,117 @@ const getTransaction = () => {
 
 }
 
+const insertTransaction = () => {
+    return new Promise((resolve,reject) => {
+
+        let loop = 0;
+        const callAPI = () => {
+            insertData.transaction_info.length == loop ? 
+            resolve() :
+            databaseInsert(insertData.transaction_info[loop++], callAPI);
+        }
+        databaseInsert(insertData.transaction_info[loop++], callAPI)
+
+    })
+}
+
+const databaseInsert = (data, callback) => {
+
+    let initiation_date = dateformat(new Date(data.transaction_info.transaction_initiation_date),'yyyy-mm-dd HH:MM:ss');
+    let updated_date = dateformat(new Date(data.transaction_info.transaction_updated_date),'yyyy-mm-dd HH:MM:ss');
+    
+    const tomodel_data = {
+        paypal_id: syncData.paypal_id,
+        transaction_id: data.transaction_info.transaction_id,
+        transaction_event_code: data.transaction_info.transaction_event_code,
+        order_number: data.transaction_info.invoice_id, 
+        initiation_date,
+        updated_date,
+        transaction_amount: Number(data.transaction_info.transaction_amount.value),
+        fee_amount:Number((data.transaction_info.hasOwnProperty('fee_amount') && data.transaction_info.fee_amount.value)) || 0,
+        transaction_status: data.transaction_info.transaction_status,
+        transaction_note:  data.transaction_info.transaction_note,
+        payer_email: data.payer_info.hasOwnProperty('email_address') && data.payer_info.email_address,
+        payer_name: data.payer_info.hasOwnProperty('payer_name') && data.payer_info.payer_name.alternate_full_name
+    }
+
+    execute(`INSERT INTO app_paypal_transaction SET ?`,
+    (err,rows)=>{
+        if ( err ) {
+           throw err;
+        } else {
+            callback();
+        }
+    }, tomodel_data);
+
+}
+
+const timeSave = () => {
+    return new Promise((resolve,reject) => {
+
+        // 실제 호출시간 맞추겠다
+        // contents.start_date = new Date(contents.start_date).setHours(new Date(contents.start_date).getHours()-9); 
+        // contents.end_date = new Date(contents.end_date).setHours(new Date(contents.end_date).getHours()-9); 
+
+        execute(`INSERT INTO app_paypal_api_history (
+                start_date,
+                end_date,
+                paypal_id,
+                count
+                ) VALUES (
+                    "${dateformat(contents.start_date, 'yyyy-mm-dd HH:MM:ss')}",
+                    "${dateformat(contents.end_date,'yyyy-mm-dd HH:MM:ss')}",
+                    ${syncData.paypal_id},
+                    ${insertData.transaction_info.length}
+                )`,
+                (err,rows)=>{
+                    if ( err ) {
+                        throw err;
+                    } else {
+                        resolve();
+                    }
+                }, {});
+    })
+}
+
+const connectionClose = (callback,bool) => {
+    return new Promise((resolve,reject) => {
+
+        console.log(new Date() + ' 종료');
+        console.log('=====================================================================');
+
+        if ( bool ) {
+            closing();
+        }
+        callback();
+    });
+}
+
 const worker = async(sync,callback,bool) => {
+
+    console.log('=====================================================================');
+    console.log(new Date() + ' 시작');
 
     syncData.paypal_id = sync.paypal_id;
     syncData.account = sync.account;
     syncData.access_token = sync.access_token;
 
+    insertData.transaction_info = [];
+
     await lastApiHistory();
     await dateCheck();
-    await getTransaction();
+    const success = await getTransaction();
 
-    // const token = sync.map(i => i.access_token);
-    // const paypalId = sync.map(i => i.paypal_id);
-    // syncData = syncData.concat({"paypal_id":sync.paypal_id, "access_token":sync.access_token});
-    // const token = sync.map(obj => Object.entries(obj));
-    // console.log("token", token)
+    if ( !success ) {
+        await connectionClose(callback,bool);
+        return;
+    }
+
+    insertData.transaction_info.length !=0 && await insertTransaction();
+
+    await timeSave();
+    await connectionClose(callback,bool);
+
 }
 
 module.exports = worker;
